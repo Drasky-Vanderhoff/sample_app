b52065b7c488b023bf8d8a6d68817189560d35ba
Drasky Vanderhoff <drasky.vanderhoff@gmail.com>
2011-02-21 00:32:37 -0300

    Added User Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SampleApp::Application.routes.draw do
+  match '/signup',    :to => 'user#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
